d8e6ee3264658627301ac24bda3b873f1790fb76
braelynmuskwe <braemira1@icloud.com>
2018-06-09 06:19:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+#the dogs name
 # # dog.rb
 # class Dog
 # def name 
@@ -14,15 +15,15 @@
 
 
 
-
- class Breed
-   def breed
-     @this_dog_breed
-   end 
+#the dogs breed
+# class Breed
+#   def breed
+#     @this_dog_breed
+#   end 
    
-   def bulldog=(breed_dog)
-     @this_dog_breed
-   end
- end
- puts"the dogs breed is a bulldog"
+#   def bulldog=(breed_dog)
+#     @this_dog_breed
+#   end
+# end
+# puts"the dogs breed is a bulldog"
 
