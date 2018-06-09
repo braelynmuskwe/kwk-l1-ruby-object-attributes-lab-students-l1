9511c2e81335cb20ec147e55a910d09c3a77100c
braelynmuskwe <braemira1@icloud.com>
2018-06-09 06:12:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
-# # dog.rb
-# class Dog
-# def name 
-#   @this_dog_name
-# end 
-# end
+# dog.rb
+class Dog
+def name 
+  @this_dog_name
+end 
+end
 
-# def name=(dog_name)
-#   tia=(dog_name)
-#   @this_dog_name
-# end 
+def tia=(dog_name)
+  tia=(dog_name)
+  @this_dog_name
+end 
 puts"the dogs name is tia"
